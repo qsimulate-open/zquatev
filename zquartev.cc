f7cf579c790a5b17e5f1de11221fd9ce19812885
Toru Shiozaki <shiozaki@northwestern.edu>
2014-01-15 23:39:15 -0600

    minor improvements due to cache efficiency.
    feel like I did all I can do...

--- zquartev.cc
+++ zquartev.cc
@@ -19,6 +19,7 @@
 #include "zquartev.h"
 #include "f77.h"
 #include <cassert>
+#include <algorithm>
 
 using namespace std;
 
@@ -51,10 +52,21 @@ void zquatev(const int n2, complex<double>* const D, double* const eig) {
   assert(n2 % 2 == 0); 
   const int n = n2/2;
 
+  // rearrange data
+  complex<double>* const D0 = D;
+  complex<double>* const D1 = D + n*n;
+  copy_n(D, n2*n, D+n2*n);
+  for (int i = 0; i != n; ++i) {
+    copy_n(D+n2*n+i*n2, n, D0+i*n);
+    copy_n(D+n2*n+i*n2+n, n, D1+i*n);
+  }
+
   // identity matrix of n2 dimension
-  complex<double>* const Q = D + n2*n; 
-  fill_n(Q, n2*n, 0.0);
-  for (int i = 0; i != n; ++i) Q[i+n2*i] = 1.0;
+  complex<double>* const Q0 = D + n2*n; 
+  complex<double>* const Q1 = D + n2*n + n*n;
+  fill_n(Q0, n*n, 0.0);
+  fill_n(Q1, n*n, 0.0);
+  for (int i = 0; i != n; ++i) Q0[i+n*i] = 1.0;
 
   unique_ptr<complex<double>[]> buf(new complex<double>[n2]);
   unique_ptr<complex<double>[]> hout(new complex<double>[n2]);
@@ -65,85 +77,85 @@ void zquatev(const int n2, complex<double>* const D, double* const eig) {
   for (int k = 0; k != n-1; ++k) {
     const int len = n-k-1;
     if (len > 1) {
-      complex<double>* const hin = D+n2*k+k+1+n;
+      complex<double>* const hin = D1+n*k+k+1;
       complex<double> tau = householder(hin, hout.get(), len);
 
       for (int i = 0; i != len; ++i) choutf[i] = conj(hout[i]);
 
       // 00-1
-      zgemv_("T", len, len+1, 1.0, D+k+1+(k)*n2, n2, hout.get(), 1, 0.0, buf.get(), 1); 
-      zgeru_(len, len+1, -conj(tau), choutf.get(), 1, buf.get(), 1, D+k+1+(k)*n2, n2);
+      zgemv_("T", len, len+1, 1.0, D0+k+1+(k)*n, n, hout.get(), 1, 0.0, buf.get(), 1); 
+      zgeru_(len, len+1, -conj(tau), choutf.get(), 1, buf.get(), 1, D0+k+1+(k)*n, n);
 
       // 00-2
-      zgemv_("N", len+1, len, 1.0, D+(k+1)*n2+(k), n2, choutf.get(), 1, 0.0, buf.get(), 1); 
-      zgeru_(len+1, len, -tau, buf.get(), 1, hout.get(), 1, D+(k+1)*n2+(k), n2);
+      zgemv_("N", len+1, len, 1.0, D0+(k+1)*n+(k), n, choutf.get(), 1, 0.0, buf.get(), 1); 
+      zgeru_(len+1, len, -tau, buf.get(), 1, hout.get(), 1, D+(k+1)*n+(k), n);
 
       // 10-1
-      zgemv_("N", len+1, len, 1.0, D+k+n+(k+1)*n2, n2, choutf.get(), 1, 0.0, buf.get(), 1); 
+      zgemv_("N", len+1, len, 1.0, D1+k+(k+1)*n, n, choutf.get(), 1, 0.0, buf.get(), 1); 
       conjarray(buf, len+1);
-      zgerc_(len, len+1, tau, hout.get(), 1, buf.get(), 1, D+k+n+1+(k)*n2, n2);
+      zgerc_(len, len+1, tau, hout.get(), 1, buf.get(), 1, D1+k+1+(k)*n, n);
 
       // 10-2
-      zgemv_("N", len+1, len, 1.0, D+k+n+(k+1)*n2, n2, choutf.get(), 1, 0.0, buf.get(), 1); 
-      zgeru_(len+1, len, -tau, buf.get(), 1, hout.get(), 1, D+(k+1)*n2+(k+n), n2);
+      zgemv_("N", len+1, len, 1.0, D1+k+(k+1)*n, n, choutf.get(), 1, 0.0, buf.get(), 1); 
+      zgeru_(len+1, len, -tau, buf.get(), 1, hout.get(), 1, D1+(k+1)*n+(k), n);
 
       // 00-2
-      zgemv_("N", len+1, len, 1.0, Q+(k+1)*n2+(k), n2, choutf.get(), 1, 0.0, buf.get(), 1); 
-      zgeru_(len+1, len, -tau, buf.get(), 1, hout.get(), 1, Q+(k+1)*n2+(k), n2);
+      zgemv_("N", len+1, len, 1.0, Q0+(k+1)*n+(k), n, choutf.get(), 1, 0.0, buf.get(), 1); 
+      zgeru_(len+1, len, -tau, buf.get(), 1, hout.get(), 1, Q0+(k+1)*n+(k), n);
 
       // 10-2
-      zgemv_("N", len+1, len, 1.0, Q+k+n+(k+1)*n2, n2, choutf.get(), 1, 0.0, buf.get(), 1); 
-      zgeru_(len+1, len, -tau, buf.get(), 1, hout.get(), 1, Q+(k+1)*n2+(k+n), n2);
+      zgemv_("N", len+1, len, 1.0, Q1+k+(k+1)*n, n, choutf.get(), 1, 0.0, buf.get(), 1); 
+      zgeru_(len+1, len, -tau, buf.get(), 1, hout.get(), 1, Q1+(k+1)*n+(k), n);
 
     }   
 
     // symplectic Givens rotation to clear out D(k+n, k)
-    pair<double,complex<double>> gr = givens(D[k+1+k*n2], D[k+n+1+k*n2]);
+    pair<double,complex<double>> gr = givens(D0[k+1+k*n], D1[k+1+k*n]);
 
-    zrot_(len+1, D+k+1+k*n2, n2, D+k+n+1+k*n2, n2, gr.first, gr.second);
-
-    for (int i = 0; i != len+1; ++i)
-      D[(k+1)*n2+k+n+i] = -conj(D[(k+1)*n2+k+n+i]);
-    zrot_(len+1, D+(k+1)*n2+k, 1, D+(k+1)*n2+k+n, 1, gr.first, conj(gr.second));
-    for (int i = 0; i != len+1; ++i)
-      D[(k+1)*n2+k+n+i] = -conj(D[(k+1)*n2+k+n+i]);
+    zrot_(len+1, D0+k+1+k*n, n, D1+k+1+k*n, n, gr.first, gr.second);
 
     for (int i = 0; i != len+1; ++i)
-      Q[(k+1)*n2+k+n+i] = -conj(Q[(k+1)*n2+k+n+i]);
-    zrot_(len+1, Q+(k+1)*n2+k, 1, Q+(k+1)*n2+k+n, 1, gr.first, conj(gr.second));
+      D1[(k+1)*n+k+i] = -conj(D1[(k+1)*n+k+i]);
+    zrot_(len+1, D0+(k+1)*n+k, 1, D1+(k+1)*n+k, 1, gr.first, conj(gr.second));
     for (int i = 0; i != len+1; ++i)
-      Q[(k+1)*n2+k+n+i] = -conj(Q[(k+1)*n2+k+n+i]);
+      D1[(k+1)*n+k+i] = -conj(D1[(k+1)*n+k+i]);
+
+    for (int i = 0; i != len+1; ++i)
+      Q1[(k+1)*n+k+i] = -conj(Q1[(k+1)*n+k+i]);
+    zrot_(len+1, Q0+(k+1)*n+k, 1, Q1+(k+1)*n+k, 1, gr.first, conj(gr.second));
+    for (int i = 0; i != len+1; ++i)
+      Q1[(k+1)*n+k+i] = -conj(Q1[(k+1)*n+k+i]);
 
     // Householder to fix top half in column k
     if (len > 1) {
-      complex<double>* const hin = D+n2*k+k+1;
+      complex<double>* const hin = D0+n*k+k+1;
       complex<double> tau = householder(hin, hout.get(), len);
 
       for (int i = 0; i != len; ++i) choutf[i] = conj(hout[i]);
 
       // 00-1
-      zgemv_("C", len, len+1, 1.0, D+k+1+(k)*n2, n2, hout.get(), 1, 0.0, buf.get(), 1); 
-      zgerc_(len, len+1, -tau, hout.get(), 1, buf.get(), 1, D+k+1+(k)*n2, n2);
+      zgemv_("C", len, len+1, 1.0, D0+k+1+(k)*n, n, hout.get(), 1, 0.0, buf.get(), 1); 
+      zgerc_(len, len+1, -tau, hout.get(), 1, buf.get(), 1, D0+k+1+(k)*n, n);
 
       // 00-2
-      zgemv_("N", len+1, len, 1.0, D+(k+1)*n2+(k), n2, hout.get(), 1, 0.0, buf.get(), 1); 
-      zgerc_(len+1, len, -conj(tau), buf.get(), 1, hout.get(), 1, D+(k+1)*n2+(k), n2);
+      zgemv_("N", len+1, len, 1.0, D0+(k+1)*n+(k), n, hout.get(), 1, 0.0, buf.get(), 1); 
+      zgerc_(len+1, len, -conj(tau), buf.get(), 1, hout.get(), 1, D0+(k+1)*n+(k), n);
 
       // 01-1
-      zgemv_("T", len, len+1, 1.0, D+k+n+1+(k)*n2, n2, hout.get(), 1, 0.0, buf.get(), 1); 
-      zgeru_(len, len+1, -conj(tau), choutf.get(), 1, buf.get(), 1, D+k+n+1+(k)*n2, n2);
+      zgemv_("T", len, len+1, 1.0, D1+k+1+(k)*n, n, hout.get(), 1, 0.0, buf.get(), 1); 
+      zgeru_(len, len+1, -conj(tau), choutf.get(), 1, buf.get(), 1, D1+k+1+(k)*n, n);
 
       // 01-2 
-      zgemv_("N", len+1, len, -1.0, D+(k+1)*n2+(k+n), n2, hout.get(), 1, 0.0, buf.get(), 1); 
-      zgerc_(len+1, len, conj(tau), buf.get(), 1, hout.get(), 1, D+(k+1)*n2+(k+n), n2);
+      zgemv_("N", len+1, len, -1.0, D1+(k+1)*n+(k), n, hout.get(), 1, 0.0, buf.get(), 1); 
+      zgerc_(len+1, len, conj(tau), buf.get(), 1, hout.get(), 1, D1+(k+1)*n+(k), n);
 
       // 00-2
-      zgemv_("N", len+1, len, 1.0, Q+(k+1)*n2+(k), n2, hout.get(), 1, 0.0, buf.get(), 1); 
-      zgerc_(len+1, len, -conj(tau), buf.get(), 1, hout.get(), 1, Q+(k+1)*n2+(k), n2);
+      zgemv_("N", len+1, len, 1.0, Q0+(k+1)*n+(k), n, hout.get(), 1, 0.0, buf.get(), 1); 
+      zgerc_(len+1, len, -conj(tau), buf.get(), 1, hout.get(), 1, Q0+(k+1)*n+(k), n);
 
       // 01-2 
-      zgemv_("N", len+1, len, -1.0, Q+(k+1)*n2+(k+n), n2, hout.get(), 1, 0.0, buf.get(), 1); 
-      zgerc_(len+1, len, conj(tau), buf.get(), 1, hout.get(), 1, Q+(k+1)*n2+(k+n), n2);
+      zgemv_("N", len+1, len, -1.0, Q1+(k+1)*n+(k), n, hout.get(), 1, 0.0, buf.get(), 1); 
+      zgerc_(len+1, len, conj(tau), buf.get(), 1, hout.get(), 1, Q1+(k+1)*n+(k), n);
 
     }   
 
@@ -151,21 +163,23 @@ void zquatev(const int n2, complex<double>* const D, double* const eig) {
 
   // diagonalize this tri-diagonal matrix (this step is much cheaper than
   // the Householder transformation above).
-  unique_ptr<complex<double>[]> Cmat(new complex<double>[n2*n2]);
+  unique_ptr<complex<double>[]> Cmat(new complex<double>[n*n]);
   unique_ptr<complex<double>[]> Work(new complex<double>[n]);
   int info;
   unique_ptr<double[]> rwork(new double[n*3]);
   for (int i = 0; i != n; ++i)
     for (int j = 0; j <= i; ++j)
-      D[i-j+j*n2] = D[i+j*n2];
-  zhbev_("V", "L", n, 1, D, n2, eig, Cmat.get(), n, Work.get(), rwork.get(), info);
+      D0[i-j+j*n] = D0[i+j*n];
+  zhbev_("V", "L", n, 1, D0, n, eig, Cmat.get(), n, Work.get(), rwork.get(), info);
   if (info) throw runtime_error("zhbev failed in quaternion diagonalization");
 
   // form the coefficient matrix in D
 #ifdef MKL
-  zgemm3m_("N", "N", n2, n, n, 1.0, Q, n2, Cmat.get(), n, 0.0, D, n2);
+  zgemm3m_("N", "N", n, n, n, 1.0, Q0, n, Cmat.get(), n, 0.0, D, n2);
+  zgemm3m_("N", "N", n, n, n, 1.0, Q1, n, Cmat.get(), n, 0.0, D+n, n2);
 #else
-  zgemm_("N", "N", n2, n, n, 1.0, Q, n2, Cmat.get(), n, 0.0, D, n2);
+  zgemm_("N", "N", n, n, n, 1.0, Q0, n, Cmat.get(), n, 0.0, D, n2);
+  zgemm_("N", "N", n, n, n, 1.0, Q1, n, Cmat.get(), n, 0.0, D+n, n2);
 #endif
 
   // eigen vectors using symmetry
